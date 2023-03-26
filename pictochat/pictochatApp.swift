b67f8404c71bdb87dcc2d3b7f08501097dc0b8b6
Tyler McCormick <tyler.mccormick@colorado.edu>
2023-03-26 16:55:22 -0600

    Initial Commit

---
+++ pictochat/pictochatApp.swift
@@ -0,0 +1,17 @@
+//
+//  pictochatApp.swift
+//  pictochat
+//
+//  Created by Tyler McCormick on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct pictochatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
